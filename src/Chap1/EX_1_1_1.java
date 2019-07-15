606907b7dc10da9f937611f039437b93804dc2c0
shaoqinzhang <1254264464@qq.com>
2019-07-15 19:31:36 +0800

    update

---
+++ src/Chap1/EX_1_1_1.java
@@ -0,0 +1,10 @@
+package Chap1;
+
+public class EX_1_1_1 {
+    public static void main(String arg[])
+    {
+        System.out.println((0 + 15) / 2);
+        System.out.println(2.0e-6 * 100000000.1);
+        System.out.println(true && false || true && true);
+    }
+}
---
+++ src/Chap1/EX_1_1_2.java
@@ -0,0 +1,11 @@
+package Chap1;
+
+public class EX_1_1_2 {
+    public static void main(String arg[])
+    {
+        System.out.println((1 + 2.236) / 2);
+        System.out.println(1 + 2 + 3 + 4.0);
+        System.out.println(4.1 >= 4);
+        System.out.println(1 + 2 + "3");
+    }
+}
